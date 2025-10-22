91b21e10b98523a802d90e2d6b58d875d23adf4a
Uros Petrovic <132246925+urosptrvc@users.noreply.github.com>
2025-10-22 21:31:12 +0200

    Update QrCode.tsx

--- src/components/QrCode.tsx
+++ src/components/QrCode.tsx
@@ -1,6 +1,8 @@
 "use client";
 import { QRCode } from "@/src/components/ui/shadcn-io/qr-code";
-const QrCodeGenerator = () => (
-  <QRCode data="http://www.localhost:3000/table=?1" />
-);
+
+const url = process.env.NEXT_PUBLIC_AUTH_TOKEN;
+
+const QrCodeGenerator = () => <QRCode data={`${url}/table=?1`} />;
+
 export default QrCodeGenerator;
